410d9195e0279327eec75ab051000fd84ae83a77
Titouan Lozac'h <titouan.lozac_h@ens-paris-saclay.fr>
2025-03-14 14:17:47 +0100

    finally (guided) eqsat as strategies + new test file created

--- src/main/scala/rise/eqsat/elevate_eqsat.scala
+++ src/main/scala/rise/eqsat/elevate_eqsat.scala
@@ -1,6 +1,6 @@
 package rise.eqsat
 
-//import elevate.core.strategies.basic._
+import elevate.core.strategies.basic.id
 //import elevate.core.strategies.traversal._
 //import elevate.core.strategies.predicate._ 
 //import elevate.core.strategies.debug._
@@ -10,69 +10,109 @@ import rise.elevate.rules.traversal._
 //import elevate.core.strategies.Traversable
 import elevate.macros.StrategyMacro
 // import scala.language.implicitConversions
+import GuidedSearch._
+import scala.language.existentials
  
 object elevate_eqsat {
 
-    // we define two different equivalence provers here : they are interesting in the context of guided eqsat where we give 
-    // complete terms as guides
-    // would be nice to have sketches instead here 
+    /*
+    equivalence prover with BENF and CNF 
+    */
 
-    // unsure I should use the macro expansion here
-    // @strategy 
     def prove_equiv_BENF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = BENF.directedRules): Rise => Strategy[Rise] =
+        // t is the target, p the input program
+        // we make it in this order to be able to compose in a nice way
         t => p => try {
-            ProveEquiv.init().runBENF(ProveEquiv.OneOrMore(Seq(t)), ProveEquiv.OneOrMore(Seq(p)), rules, normRules);
-            Success(p)
+            ProveEquiv.init().runBENF(ProveEquiv.OneOrMore(Seq(p)), ProveEquiv.OneOrMore(Seq(t)), rules, normRules);
+            Success(t)
         } catch {
             case _ : Exception => Failure(prove_equiv_BENF(rules,normRules)(t)) //CouldNotProveEquiv
         }
     
-    // unsure I should use the macro expansion here
-    // @strategy 
     def prove_equiv_CNF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = BENF.directedRules): Rise => Strategy[Rise] =
+        // t is the target, p the input program
+        // we make it in this order to be able to compose in a nice way
         t => p => try {
-            ProveEquiv.init().runCNF(ProveEquiv.OneOrMore(Seq(t)), ProveEquiv.OneOrMore(Seq(p)), rules, normRules);
-            Success(p)
+            ProveEquiv.init().runCNF(ProveEquiv.OneOrMore(Seq(p)), ProveEquiv.OneOrMore(Seq(t)), rules, normRules);
+            Success(t)
         } catch {
             case _ : Exception => Failure(prove_equiv_CNF(rules, normRules)(t)) //CouldNotProveEquiv
         }
-
-        // idem + informal : it can't work because I am not using any new guides at all
-        // I need to declare some function f : Int => D with D the nice domain such that
-        // on input n, f creates a new function g taking as an input a vector x of n terms 
-        // and apply the prove_equiv n times with x[i] and x[i+1] as terms
-        // if it fails at some point, then fail
-    // @strategy def N_prove_equiv_CNF(rules: Seq[Rewrites], normRules: Seq[RewriteDirected] = BENF.directedRules, n: int): Rise => Strategy[Rise] = 
-     //   t => p => if (n>0) then {prove_equiv_CNF(rules, normRules)(t,p) |> N_prove_equiv_CNF(rules, normRules, n-1)(p)} 
-     //   else {id(p)}
-
-    // it is not so satisfying in terms of the syntax. but i need some way to have guides ... 
-    // we only deal with concrete terms here. need to implement the same thing for sketches also
-    def guided_prove_equiv_CNF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = CNF.directedRules, l: List[Rise]) : RewriteResult[Rise] = 
-        l match {
-            case Nil => Failure(_ => guided_prove_equiv_CNF(rules, normRules, l));
-            case head :: Nil => Failure(head => guided_prove_equiv_CNF(rules, normRules, l));
-            case t :: p :: Nil => prove_equiv_CNF(rules, normRules)(t)(p);
-            case t :: p :: tail => prove_equiv_CNF(rules, normRules)(t)(p) match {
-                case Failure(x) => Failure(x);
-                case Success(term) => guided_prove_equiv_CNF(rules, normRules, (p::tail));
-            }
-        }
     
-    def guided_prove_equiv_BENF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = BENF.directedRules, l: List[Rise]) : RewriteResult[Rise] = 
-        l match {
-            case Nil => Failure(_ => guided_prove_equiv_BENF(rules, normRules, l));
-            case head :: Nil => Failure(head => guided_prove_equiv_BENF(rules, normRules, l));
-            case t :: p :: Nil => prove_equiv_BENF(rules, normRules)(t)(p);
-            case t :: p :: tail => prove_equiv_BENF(rules, normRules)(t)(p) match {
-                case Failure(x) => Failure(x);
-                case Success(_) => guided_prove_equiv_BENF(rules, normRules, (p::tail));
+    /* 
+    classic equality saturation, with CNF and BENF
+    */
+
+    def runCNF: Strategy[Rise] = p => Success(Expr.toNamed(CNF(Expr.fromNamed(p))))
+
+    def runBENF: Strategy[Rise] = p => Success(Expr.toNamed(BENF(Expr.fromNamed(p))._1))
+
+    /* 
+    term-guided equality saturation
+    */
+
+    def termguided_pe_CNF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = CNF.directedRules, l:List[Rise]) : Strategy[Rise] = 
+        // l is the list of guides
+        l.foldLeft(id[Rise])((s,t) => s `;` prove_equiv_CNF(rules, normRules)(t))
+
+    def termguided_pe_BENF(rules: Seq[Rewrite], normRules: Seq[RewriteDirected] = BENF.directedRules, l:List[Rise]) : Strategy[Rise] = 
+        // l is the list of guides
+        l.foldLeft(id[Rise])((s,t) => s `;` prove_equiv_BENF(rules, normRules)(t))
+
+    /*
+    sketch-guided equality saturation 
+    */
+
+    def runOne_acc(filter: Predicate, transformRunner: Runner => Runner, step: GuidedSearch.Step) : Strategy[Rise] = 
+    t => {
+        val start = Expr.fromNamed(t)
+        val startTime = System.nanoTime()
+        // note: this is a bit hacky
+        val timeLimit = transformRunner(Runner.init()).timeLimit
+
+        var normRewriteCount = 0L
+        val (initializeTime, (egraph, rootId)) = util.time{
+        val egraph = EGraph.empty()
+        val beam = Seq(start)
+        val normBeam = beam.map { e =>
+            val (n, rc) = step.normalForm.normalizeCountRewrites(e)
+            normRewriteCount += rc
+            n
             }
+        val rootId = normBeam.map(egraph.addExpr)
+                    .reduce[EClassId] { case (a, b) => egraph.union(a, b)._1 }
+                egraph.rebuild(Seq(rootId))
+                (egraph, rootId)
         }
 
-    //we want something like Success(eqsat'(t)) where eqsat'(t) is just the best term found by eqsat
-    // @strategy 
-    def eqsat(iterations: Int): Strategy[Rise] = t => Success(t) 
+        // TODO: add goal check to e-graph for incremental update?
+        val mergedRules = (step.rules ++ step.normalForm.rules).distinctBy(_.name)
+        val (growTime, runner) = util.time(transformRunner(Runner.init())
+            // note: update time limit
+            .withTimeLimit(java.time.Duration.ofNanos(timeLimit - (System.nanoTime() - startTime)))
+            .doneWhen { _ =>
+            util.printTime("goal check", Sketch.exists(step.sketch, egraph, rootId))
+            }.run(egraph, filter, mergedRules, Seq(), Seq(rootId)))
+        val found = runner.stopReasons.contains(Done)
+
+        val (extractionTime, newBeam) = if (found) {
+                util.time(step.extractor.extract(step.sketch, egraph, rootId))
+            } else {
+                (0L, Seq())
+            }
+        if (found) {
+            assert(newBeam.nonEmpty)
+            Success(Expr.toNamed(newBeam(0))) // one corresponding term
+        } else {
+            Failure(runOne_acc(filter, transformRunner, step)) // could not reach sketch
+        }
+    }
+
+    def runOne(filter: Predicate, transformRunner: Runner => Runner, nf : NF, rules: Seq[Rewrite] = Seq(), ex: Extractor = BeamExtractor(1, AstSize), sketch: Sketch = SketchAny(TypePatternAny)) : Strategy[Rise] = 
+        runOne_acc(filter, transformRunner, Step.init(nf).withRules(rules).withExtractor(ex).withSketch(sketch))
+
+    def guided_eqsat(filter: Predicate, transformRunner: Runner => Runner, nf : NF, rules: Seq[Rewrite] = Seq(), ex: Extractor = BeamExtractor(1, AstSize), sketches: List[Sketch] = List(SketchAny(TypePatternAny))) : Strategy[Rise] = 
+        sketches.foldLeft(id[Rise])((s,t) => s `;` runOne(filter, transformRunner, nf, rules, ex, t))
+
 
 }
-
---
+++ src/test/scala/rise/eqsat/ElevateBasic.scala
@@ -0,0 +1,45 @@
+package rise.eqsat
+
+import ProveEquiv.syntax._
+
+class ElevateBasic extends test_util.Tests {
+    import rise.core.DSL._
+    import rise.core.DSL.Type._
+    import rise.core.primitives._
+
+    test("prove_equiv_BENF"){
+        //TODO
+    }
+
+    test("prove_equiv_CNF"){
+        //TODO
+    }
+
+    test("runCNF"){
+        //TODO
+    }
+
+    test("runBENF"){
+        //TODO
+    }
+    
+    test("termguided_pe_BENF"){
+        //TODO
+    }
+
+    test("termguided_pe_CNF"){
+        //TODO
+    }
+
+    test("runOne_acc"){
+        //TODO
+    }
+
+    test("runOne"){
+        //TODO
+    }
+
+    test("guided_eqsat"){
+        //TODO
+    }
+}
